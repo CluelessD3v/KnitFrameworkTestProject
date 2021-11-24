f9cbabe2da2773a81735c05137d53c94ee16ed9c
CluelessD3v <cluelessdevmail@gmail.com>
2021-11-24 07:47:15 -0500

    Wrap SpawnKillBricks methods around a task.spawn

--- src/Server/Components/Killbrick.lua
+++ src/Server/Components/Killbrick.lua
@@ -1,18 +1,23 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Maid = require(ReplicatedStorage.Packages.maid)
 
-local Killbrick = {}
-Killbrick.__index = Killbrick
+local KillBrick = {}
+KillBrick.__index = KillBrick
 
-Killbrick.Tag = "Killbrick"
+KillBrick.Tag = "KillBrick"
 
-function Killbrick.new(instance)
-    local self = setmetatable({}, Killbrick)
+function KillBrick.new(instance)
+    local self = setmetatable({}, KillBrick)
     print("Killbrick created")
 
     self.Maid = Maid.new()
     self.Instance = instance
     self.Maid:AddTask(self.Instance)
+
+    self.Instance.Size = Vector3.new(4,4,4)
+    self.Instance.Material = Enum.Material.Neon
+    self.Instance.BrickColor = BrickColor.new("Really red")
+    self.Instance.Parent = workspace
 
     self.Maid:AddTask(function()
         print("destroyed")
@@ -20,30 +25,29 @@ function Killbrick.new(instance)
     return self
 end
 
-function Killbrick:Init()
-    print("Init")
-
+function KillBrick:Init()
      self.Maid:AddTask(self.Instance.Touched:Connect(function(theTouchedPart)
         local db: boolean = false
         
         if db == false then
             db = true
-            
             local humanoid: Humanoid = theTouchedPart.Parent:FindFirstChild("Humanoid") 
+
             if humanoid then
                 humanoid.Health = 0
                 self.Maid:Cleanup()
             end
+            
 
             task.wait(1)
-            db = true
-        end
+            db = false
+        end   
     end))
 end
 
-function Killbrick:Destroy()
+function KillBrick:Destroy()
     self.Maid:Cleanup()
 end
 
-return Killbrick
+return KillBrick
 
--- src/Server/Services/WorldService.lua
+++ src/Server/Services/WorldService.lua
@@ -1,13 +1,12 @@
 --[[
     This service is the class responsible for core game functionality
 ]]
-
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 local ServerScriptService = game:GetService("ServerScriptService")
+local CollectionService = game:GetService("CollectionService")
 local Players = game:GetService("Players")
-
 local PlayerEntity = require(ServerScriptService.PlayerEntitiy)
-
 local WorldService = Knit.CreateService {
     Name = "World";
     Client = {};
@@ -31,10 +30,23 @@ function WorldService:RemovePlayerEntity()
     end)
 end
 
-function WorldService:KnitInit()
-    self:AddPlayerEntity()
-    self:RemovePlayerEntity()
+function WorldService:SpawnKillBricks()
+    for _ = 1, 10 do
+        task.wait(.3)
+        local xOffset = math.random(0, 100)
+        local zOffset = math.random(0, 100)
+        local newKillbrick = Instance.new("Part")
+        CollectionService:AddTag(newKillbrick, "KillBrick")
+        newKillbrick.Position = Vector3.new(xOffset, 100, zOffset)
+        newKillbrick.Parent = workspace
+    end
 end
 
+function WorldService:KnitStart()
+    self:AddPlayerEntity()
+    self:RemovePlayerEntity()
+    task.spawn(self.SpawnKillBricks, self)
+end
+    
 
 return WorldService
